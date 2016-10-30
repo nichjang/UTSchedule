48f74e7ffa30cdcda9397ff15eb24d0c2c0705b9
nichjang <nich.jang@gmail.com>
2016-10-29 22:50:48 -0500

    Added sorting

--- API.py
+++ API.py
@@ -1,5 +1,5 @@
 from flask import request, url_for
-from flask.ext.api import FlaskAPI
+from flask_api import FlaskAPI
 import postgresql
 import json 
 
@@ -17,8 +17,27 @@ def get_classes():
 @app.route('/get/info/<string:name>')
 def get_info(name):
 	# WHERE name='{}'".format(str(name))
-	ps = db.prepare("SELECT * FROM masterlist WHERE name LIKE '{}'".format(str(name)))
-	print("SELECT * FROM masterlist WHERE name = '{}'".format(str(name)))
+	ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' ORDER BY uniqueid ASC".format(len(name),str(name)))
+	print("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}'".format(len(name),str(name)))
+	return (json.dumps(ps()))
+
+@app.route('/get/info/<string:name>/')
+def get_sortedinfo(name):
+	#use time to filter results by specified time
+	#use select: 0=all classes sorted by time, 1=only classes <= time, 2=only classes >= time
+	intime = request.args.get('time')
+	select = request.args.get('select')
+	print(intime)
+	print(select)
+	if int(select)==0:
+		ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' ORDER BY to_timestamp(starttime,'HH:MIAM') ASC".format(len(name),str(name)))
+	elif int(select)==1:
+		print("chosen 1")
+		ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' AND to_timestamp(starttime,'HH:MIAM')::time <= '{}' ORDER BY to_timestamp(starttime,'HH:MIAM') ASC"
+			.format(len(name),str(name),str(intime)))
+	else:
+		ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' AND to_timestamp(starttime,'HH:MIAM')::time >= '{}' ORDER BY to_timestamp(starttime,'HH:MIAM') ASC"
+			.format(len(name),str(name),str(intime)))
 	return (json.dumps(ps()))
 
 if __name__ == "__main__":
