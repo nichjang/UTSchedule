59e6707ae79f647b5ca03ea35081ad960e2378ea
Nicholas Jang <nich.jang@gmail.com>
2016-10-30 13:48:52 -0500

    Update API.py

--- API.py
+++ API.py
@@ -18,7 +18,7 @@ def get_classes():
 def get_info(name):
 	# WHERE name='{}'".format(str(name))
 	ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' ORDER BY uniqueid ASC".format(len(name),str(name)))
-	print("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}'".format(len(name),str(name)))
+	#print("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}'".format(len(name),str(name)))
 	return (json.dumps(ps()))
 
 @app.route('/get/info/<string:name>/')
@@ -27,12 +27,9 @@ def get_sortedinfo(name):
 	#use select: 0=all classes sorted by time, 1=only classes <= time, 2=only classes >= time
 	intime = request.args.get('time')
 	select = request.args.get('select')
-	print(intime)
-	print(select)
 	if int(select)==0:
 		ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' ORDER BY to_timestamp(starttime,'HH:MIAM') ASC".format(len(name),str(name)))
 	elif int(select)==1:
-		print("chosen 1")
 		ps = db.prepare("SELECT * FROM masterlist WHERE substring(name FROM 1 FOR {})= '{}' AND to_timestamp(starttime,'HH:MIAM')::time <= '{}' ORDER BY to_timestamp(starttime,'HH:MIAM') ASC"
 			.format(len(name),str(name),str(intime)))
 	else:
